dce88d574eee8f07784920f7bcf132d1365ada53
ilay <cc@ilay.cc>
2020-07-30 22:01:10 +0800

    更新url

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 mkdir c:\temp1
 cd /d c:\temp1
-powershell (new-object system.Net.webclient).DownloadFile('https://raw.githubusercontent.com/cnravin/hosts_update/master/update_host.ps1', 'update_hosts.ps1')
+powershell (new-object system.Net.webclient).DownloadFile('https://raw.githubusercontent.com/cnravin/hosts_update/master/update_hosts.ps1', 'update_hosts.ps1')
 powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 powershell -f update_hosts.ps1
 cd /d c:\
