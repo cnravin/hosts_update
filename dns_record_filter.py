d0b868247adcd06bbefcf12ebcc0884f7e1f7e53
ilay <cc@ilay.cc>
2020-08-03 18:20:51 +0800

    实例化 dns.resolver.Resolver()对象，制定使用内部dns

--- dns_record_filter.py
+++ dns_record_filter.py
@@ -12,16 +12,19 @@ with open('domain_list.txt', encoding='utf-8') as f:
         domain_list.append(each_line)
 
 dns_record = dict.fromkeys(domain_list)
+dc_dns = ['10.1.120.241']
 # print(dns_record)
 
 for a in dns_record:
-
-    result = dns.resolver.resolve(a)
+    dc_resolver = dns.resolver.Resolver()
+    dc_resolver.nameservers = dc_dns
+    result = dc_resolver.resolve(a)
     for i in result.response.answer:
         for j in i.items:
             # print(j)
             add = str(j)
             dns_record[a] = add
+            # print(add)
 
 # print(dns_record)
 hosts_text = []
@@ -39,8 +42,3 @@ try:
 
 except OSError as reason:
     print('写入文件报错：%s' % reason)
-
-
-
-
-
