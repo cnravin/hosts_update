b114f0ec7f07ed842eff72b966ef78b9855558cd
ilay <cc@ilay.cc>
2020-08-03 21:48:13 +0800

    本地更新源更新脚本

---
+++ local_update/update_local.bat
@@ -0,0 +1,9 @@
+mkdir c:\temp1
+cd /d c:\temp1
+powershell (new-object system.Net.webclient).DownloadFile('https://it.digitalchina.com/dcit/hosts/update_hosts.ps1', 'update_hosts.ps1')
+powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+powershell -f update_hosts.ps1
+cd /d c:\
+del c:\temp1\*.* /s /q
+rd c:\temp1 /s /q
+@pause
---
+++ local_update/update_local.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+
+import urllib.request
+import os
+import shutil
+
+HOST_URL = 'https://it.digitalchina.com/dcit/hosts/hosts.txt'
+# HOST_URL = 'https://raw.githubusercontent.com/cnravin/hosts/master/hosts.txt'
+LOCAL_PATH = r'C:\Windows\System32\drivers\etc\hosts'
+
+
+def main():
+    urllib.request.urlretrieve(HOST_URL, 'hosts')
+    # f = open('hosts','rb')
+    # content = f.read()
+    # hs = open(LOCAL_PATH,'wb')
+    # hs.write(content)
+    # hs.close()
+    # f.close()
+
+    if os.path.exists(LOCAL_PATH + '.bak'):
+        os.remove(LOCAL_PATH)
+    else:
+        shutil.copyfile(LOCAL_PATH, LOCAL_PATH + '.bak')
+
+    shutil.move('hosts', LOCAL_PATH)
+    os.system('ipconfig /flushdns')
+
+
+if __name__ == '__main__':
+    main()
