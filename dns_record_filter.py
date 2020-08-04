b87a645a22de4eba28ee7f912cf6028d0110d6a8
ilay <cc@ilay.cc>
2020-08-04 17:35:23 +0800

    将操作移至函数内部

--- dns_record_filter.py
+++ dns_record_filter.py
@@ -5,51 +5,56 @@ import dns.resolver
 import push_hosts_update
 
 
-with open('domain_list.txt', encoding='utf-8') as f:
-    domain_list = []
-    for each_line in f:
-        p = each_line.rfind('com')
-        each_line = each_line[:p+3]
-        # print(each_line)
-        domain_list.append(each_line)
+def host_record_update():
 
-dns_record = dict.fromkeys(domain_list)
-dc_dns = ['10.1.120.241', '10.1.124.241']
-# print(dns_record)
+    with open('domain_list.txt', encoding='utf-8') as f:
+        domain_list = []
+        for each_line in f:
+            p = each_line.rfind('com')
+            each_line = each_line[:p+3]
+            # print(each_line)
+            domain_list.append(each_line)
 
-for a in dns_record:
-    dc_resolver = dns.resolver.Resolver()
-    dc_resolver.nameservers = dc_dns
-    result = dc_resolver.resolve(a)
-    for i in result.response.answer:
-        for j in i.items:
-            # print(j)
-            add = str(j)
-            dns_record[a] = add
-            # print(add)
+    dns_record = dict.fromkeys(domain_list)
+    dc_dns = ['10.1.120.241', '10.1.124.241']
+    # print(dns_record)
 
-# print(dns_record)
-hosts_text = []
-update_time = datetime.datetime.now()
+    for a in dns_record:
+        dc_resolver = dns.resolver.Resolver()
+        dc_resolver.nameservers = dc_dns
+        result = dc_resolver.resolve(a)
+        for i in result.response.answer:
+            for j in i.items:
+                # print(j)
+                add = str(j)
+                dns_record[a] = add
+                # print(add)
 
-for each in dns_record:
-    host_line = '{}\t{}\n'.format(dns_record[each], each)
-    # print(host_line)
-    hosts_text.append(host_line)
+    # print(dns_record)
+    hosts_text = []
+    update_time = datetime.datetime.now()
 
-if sys.platform == 'win32':
-    file_path = r'E:\py_project\hosts\hosts.txt'
-else:
-    file_path = '/opt/hosts/hosts.txt'
+    for each in dns_record:
+        host_line = '{}\t{}\n'.format(dns_record[each], each)
+        # print(host_line)
+        hosts_text.append(host_line)
 
-try:
-    with open(file_path, 'w') as host_file:     # 替换hosts文件存放的实际路径
-        hosts_text.insert(0, '# {}\n'.format(update_time))  # 行首插入时间戳
-        host_file.writelines(hosts_text)
-        host_file.close()
+    if sys.platform == 'win32':
+        file_path = r'E:\py_project\hosts\hosts.txt'
+    else:
+        file_path = '/opt/hosts/hosts.txt'
 
-except OSError as reason:
-    print('写入文件报错：%s' % reason)
+    try:
+        with open(file_path, 'w') as host_file:     # 替换hosts文件存放的实际路径
+            hosts_text.insert(0, '# {}\n'.format(update_time))  # 行首插入时间戳
+            host_file.writelines(hosts_text)
+            host_file.close()
+
+    except OSError as reason:
+        print('写入文件报错：%s' % reason)
+
 
 # push update to git
-push_hosts_update.push_hosts()
+if __name__ == '__main__':
+    host_record_update()
+    push_hosts_update.push_hosts()
