111b5a28672e9b386d9a9da1e44c1c1a1cf68303
ilay <cc@ilay.cc>
2020-08-04 18:18:13 +0800

    更新ps1文件本地下载路径

--- local_update/update_local.bat
+++ local_update/update_local.bat
@@ -1,6 +1,6 @@
 mkdir c:\temp1
 cd /d c:\temp1
-powershell (new-object system.Net.webclient).DownloadFile('https://it.digitalchina.com/dcit/hosts/update_hosts.ps1', 'update_hosts.ps1')
+powershell (new-object system.Net.webclient).DownloadFile('https://it.digitalchina.com/dcit/hosts/update_hosts_local.ps1', 'update_hosts.ps1')
 powershell Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 powershell -f update_hosts.ps1
 cd /d c:\
