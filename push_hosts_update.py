3ff6380706779cba09eb0a1a0e930c79e5763bdf
ilay <cc@ilay.cc>
2020-08-05 10:25:59 +0800

    push前先pull

--- push_hosts_update.py
+++ push_hosts_update.py
@@ -11,12 +11,13 @@ def push_hosts():
     # get hosts information from dns query, then schedule push it to github
 
     if sys.platform == 'win32':
-        host_path = r'E:\py_project\hosts'
+        host_path = r'D:\py_project\hosts'
 
     else:
         host_path = '/opt/hosts'
 
     os.chdir(host_path)
+    os.system('git pull')
     os.system('git add hosts.txt')
     os.system('git commit -m "update host {}"'.format(time.strftime('%Y-%m-%d %H:%M:%S')))
     time.sleep(5)
